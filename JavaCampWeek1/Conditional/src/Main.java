1bf7dbe119d8b32a21a30c035669cd09b0ee857b
Ömer Balkaya <balkayaomer26@gmail.com>
2022-09-21 00:29:55 +0300

    startup projects

---
+++ JavaCampWeek1/Conditional/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+     int sayı = 1;
+     //
+     if(sayı>17){
+         System.out.println("The number is greater than seventeen");
+     }
+         if(sayı<17){
+            System.out.println("The number is less than seventeen");
+     }
+            else if(sayı==17){
+                 System.out.println("The number is equal to seventeen");
+     }
+
+
+
+
+
+
+
+
+
+
+    }
+}
