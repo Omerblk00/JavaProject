48c7204b998ae34f947cdba3b7162757183fceec
Ömer Balkaya <balkayaomer26@gmail.com>
2022-09-21 00:06:17 +0300

    silindi

---
+++ JavaCampWeek1/Variables/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayı = 12;
+        String bilgi = "Öğrenci Numarası :";
+        System.out.println( bilgi+ sayı);
+        System.out.println( bilgi+ sayı);
+        System.out.println( bilgi+ sayı);
+        System.out.println( bilgi+ sayı);
+
+
+
+
+
+    }
+}
